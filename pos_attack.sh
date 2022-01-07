c22d6601c01a62c859a7487f1f9f53e50522ff6c
MohammadrezaQaraei <79269633+MohammadrezaQaraei@users.noreply.github.com>
2022-01-07 13:24:27 +0200

    Update pos_attack.sh

--- pos_attack.sh
+++ pos_attack.sh
@@ -4,16 +4,16 @@ data_path=data_defense/data/aplc/Wiki10/None_bins_100_neg_10
 model_path=models/Wiki10/aplc/None
 result_path=data_defense/results/aplc/Wiki10/None_bins_100_pos_10
 
-#echo "Preprocesing data"
-#python ./utils/data_utils.py \
-#--pos_label 30 \
-#--top 5 \
-#--max_neg_samples 10 \
-#--max_seq_length 512 \
-#--data_path $data_path \
-#--model_path $model_path \
-#--model_type aplc \
-#--bin_size 100
+echo "Preprocesing data"
+python ./utils/data_utils.py \
+--pos_label 30 \
+--top 5 \
+--max_neg_samples 10 \
+--max_seq_length 512 \
+--data_path $data_path \
+--model_path $model_path \
+--model_type aplc \
+--bin_size 100
 
 mkdir -p $result_path
 
